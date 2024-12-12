1f436d12503c886a2f4dbe76af972613e5cd0160
Aleksej Dix <git@aleksejdix.com>
2024-12-12 18:12:11 +0100

    drop nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxthq/studio"],
-  future: {
-    compatibilityVersion: 4,
-  },
   compatibilityDate: "2024-12-12",
 });
